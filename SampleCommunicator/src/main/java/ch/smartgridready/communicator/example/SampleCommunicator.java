66b79259fa727b989ce8c30304b14c46533f3cdc
Hans Furrer <hfurrer@ergonomics.ch>
2022-07-04 14:24:43 +0200

    Added Javadoc and inline documentation to the SampleCommunicator. Added logger to the SampleCommunicator. Work on SGr-Architecture-Overview.

--- SampleCommunicator/src/main/java/ch/smartgridready/communicator/example/SampleCommunicator.java
+++ SampleCommunicator/src/main/java/ch/smartgridready/communicator/example/SampleCommunicator.java
@@ -23,48 +23,106 @@ package ch.smartgridready.communicator.example;
 
 import com.smartgridready.ns.v0.SGrModbusDeviceDescriptionType;
 
-import communicator.common.runtime.GenDriverAPI4Modbus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.smartgridready.sgr.ns.v0.SGrModbusDeviceDescriptionType;
 import communicator.helper.DeviceDescriptionLoader;
 import communicator.helper.GenDriverAPI4ModbusRTUMock;
 import communicator.impl.SGrModbusDevice;
 import de.re.easymodbus.adapter.GenDriverAPI4ModbusTCP;
 
+
+/** 
+ * <p>
+ * This is a sample implementation of communicator that uses the SmartGridready communication
+ * handler. The communication handler uses the SmartGridready generic interface to communicate
+ * with any attached device/product in a generic way. The communication handler converts the
+ * generic API commands to commands understood by the external interface of the device/product.
+ * <p> 
+ * The command translation is done by the communication handler based on a device description
+ * loaded from a device description XML file.
+ * <p>
+ * There are several communication protocols/technologies available to communicate with the device:
+ * <ul>  
+ * 		<li>Modbus RTU</li>
+ * 		<li>Modbus TCP</li>
+ * 		<li>http / REST Swagger</li>
+ * </ul> 
+ * The communicator is responsible instantiate/load the suitable driver for the attached 
+ * devices/products.  
+ * <p>
+ * The sample shows the basic steps to set up the communicator to talk to a simple 
+ * SmartGridready Modbus device and read a value from the device.
+ * 
+ **/
 public class SampleCommunicator {
 	
-	private static final String XML_BASE_DIR = "../../SGrSpecifications/XMLInstances/ExtInterfaces/"; 
+	private static final Logger LOG =  LoggerFactory.getLogger(SampleCommunicator.class);
+	
+	private static final String XML_BASE_DIR = "../../SGrSpecifications/XMLInstances/ExtInterfaces/";
 	
 	public static void main( String argv[] ) {				
 		
 		try {	
-			
+			 
+			// Step 1: 
+			// Use the DeviceDescriptionLoader class to Load the device description from an XML file.
+			//
 			DeviceDescriptionLoader<SGrModbusDeviceDescriptionType> loader = new DeviceDescriptionLoader<>();
 			SGrModbusDeviceDescriptionType sgcpMeter = loader.load( XML_BASE_DIR, "SGr_04_0016_xxxx_ABBMeterV0.2.1.xml");
 			
-			GenDriverAPI4Modbus mbRTU = new GenDriverAPI4ModbusRTUMock();
-			GenDriverAPI4Modbus mbTCP = new GenDriverAPI4ModbusTCP();
+			// Step 2: 
+			// Load the suitable device driver to communicate with the device. The example below uses
+			// mocked driver for modbus RTU.
+			//
+			// Change the the driver to the real driver, suitable for your device. For example:
+			// - GenDriverAPI4Modbus mbTCP = new GenDriverAPI4ModbusTCP();
+			// - GenDriverAPI4Modbus mbRTU = new GenDriverAPI4ModbusRTU();
+			//
+			GenDriverAPI4Modbus mbRTUMock = new GenDriverAPI4ModbusRTUMock();
 			
-			SGrModbusDevice sgcpDevice = new SGrModbusDevice(sgcpMeter, mbRTU );
-						
-			try {
+			// Step 2a (Modbus RTU only):
+			// Initialise the serial COM port used by the modbus transport service.
+			//
+			mbRTUMock.initTrspService("COM9");
 				
-				mbRTU.setUnitIdentifier((byte) 11);
+			// Step 3:
+			// Instantiate a modbus device. Provide the device description and the device driver
+			// instance to be used for the device.
+			SGrModbusDevice abbMeterNo1 = new SGrModbusDevice(sgcpMeter, mbRTUMock );			
+			try {				
+				
+				// Step 4 (Modbus RTU only):
+				// Set the unit identifier of the device to read out. 
+				mbRTUMock.setUnitIdentifier((byte) 11);
+				
+				// Step 5: 
+				// Read the values from the device. 
+				// - "ActiveEnerBalanceAC" is the name of the functional profile.
+				// - "ActiveImportAC", ActiveExportAC and ActiveNetAC are the names of the Datapoints that
+				//   report the values corresponding to their names.
+				// 
+				// Hint: You can only read values for functional profiles and datapoints that exist 
+				// in the device description XML.
+				//
 				String Val1 = sgcpDevice.getVal("CurrentAC", "CurrentACL1");
 				String Val2 = sgcpDevice.getVal("CurrentAC", "CurrentACL2");
 				String Val3 = sgcpDevice.getVal("CurrentAC", "CurrentACL3");
 				String Val4 = sgcpDevice.getVal("CurrentAC", "CurrentACN");
 				System.out.printf("ABBMeter ActiveEnerBalanceAC [KWh]:  " + Val1 + ",  " + Val2 + ",  " + Val3 + ", " + Val4 + " %n");
-
+				
+				LOG.info("ABBMeter ActiveEnerBalanceAC [KWh]: acImport={} acExport={}  acNet={} \n", acImport, acExport, acNet);	
 			}
 			catch ( Exception e)
 			{
-				System.out.println( "Error reading value from device. " + e);
-				e.printStackTrace();
-			}
+				LOG.error( "Error reading value from device. ", e);
+			}									 									
+			
 		}
 		catch ( Exception e )
 		{
-			System.out.println( "Error loading device description. " + e);
+			LOG.error( "Error loading device description. ", e);
 		}									
-	}
-		
+	}		
 }
